0d343b0174c89a8e72b7e073efb7f1aebe7ac936
Robbie Johnson <robbiej@DCC>
2018-06-02 08:39:33 +0200

    Add project files.

---
+++ RPAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPAPI.Models;
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    public DbSet<Value> Values { get; set; }
+}
