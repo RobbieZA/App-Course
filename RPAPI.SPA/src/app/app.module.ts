4c823cca3aad185abd79e77cb152c5962b4d13a4
Robbie <robbie@retailpulse.co.za>
2018-06-13 06:31:09 +0200

    Added routing

--- RPAPI.SPA/src/app/app.module.ts
+++ RPAPI.SPA/src/app/app.module.ts
@@ -11,6 +11,12 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -18,18 +24,22 @@ import { AlertifyService } from './_services/alertify.service';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    UserListComponent,
+    ListsComponent,
+    MessagesComponent
 ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AuthService,
-    AlertifyService
-  ],
-  bootstrap: [AppComponent]
+    AlertifyService,
+    AuthGuard],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ RPAPI.SPA/src/app/routes.ts
@@ -0,0 +1,24 @@
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { Routes } from '@angular/router';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'users', component: UserListComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'list', component: ListsComponent}
+        ]
+    }
+    {path: 'users', component: UserListComponent, canActivate: [AuthGuard]},
+    {path: 'messages', component: MessagesComponent},
+    {path: 'list', component: ListsComponent},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
